6b3080061240798783a663efbc0f7d46c86e9235
Amanda KIBENDE <contactproamanda@gmail.com>
2021-06-30 15:13:38 +0200

    index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 
 // Iteration 1: Names and Input
 
+let hacker1 = "Amanda";
+console.log(`The driver name is ${hacker1}`);
+
+let hacker2 = "Zsolt";
+console.log(`The navigator name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
 
 // Iteration 3: Loops
-
-console.log("hello world")
