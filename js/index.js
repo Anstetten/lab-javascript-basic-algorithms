deebd1fe52b4f0ac6948d85643818b82cd49928a
Anstetten <h.zsola@gmail.com>
2021-06-30 15:26:16 +0200

    Commit N°3 - Iteration N°2 completed

--- js/index.js
+++ js/index.js
@@ -9,5 +9,19 @@ console.log(`The navigator name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+if (hacker1.length>hacker2.length){
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
+}
+
+else if(hacker1.length<hacker2.length){
+    console.log(`The navigator has the longest name, it has ${hacker2.length} characters`);
+}
+
+else{
+    console.log("Wow, you both have equally long names, XX characters!");
+}
+
+
+
 
 // Iteration 3: Loops
